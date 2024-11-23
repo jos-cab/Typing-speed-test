0b6774ced56e9b8bfda7bc7555fd301885aacecf
jos-cabrera <joseacabrera@protonmail.com>
2024-11-23 13:44:37 -0300

    add main components

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,27 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1 className="main-title">Typing test</h1>
+        <div className="container">
+          <button className="test-time-btn">15</button>
+          <button className="test-time-btn">30</button>
+          <button className="test-time-btn">60</button>
+          <button className="test-time-btn">120</button>
+        </div>
+      </header>
+      <main>
+        <div className="stats container">
+          <span className="time"></span>
+          <span className="wpm"></span>
+          <span className="accuracy"></span>
+        </div>
+        <textarea name="typing-area"></textarea>
+      </main>
+    </>
+  );
 }
 
 export default App;

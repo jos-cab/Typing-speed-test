0549e9e204575fb6b5f0b54053a760318ff494f2
jos-cabrera <joseacabrera@protonmail.com>
2025-08-17 13:42:20 -0300

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "https://jos-cabrera.github.io/Typing-speed-test/",
+	base: 'https://jos-cab.github.io/Typing-speed-test/',
 	css: {
 		preprocessorOptions: {
 			scss: {
-				api: "modern-compiler", // or "modern"
+				api: 'modern-compiler', // or "modern"
 			},
 		},
 	},
